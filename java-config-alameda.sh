bc0650cccf5b3d45daaa94a27cc266208c50b901
marcelojcn <marcelojcnunes@hotmail.com>
2018-04-19 16:21:44 +0100

    fixe commit

--- java-config-alameda.sh
+++
@@ -1,9 +0,0 @@
-export JAVA_HOME=/etc/alternatives/java_sdk_1.7.0
-export JAVA_ROOT=/etc/alternatives/java_sdk_1.7.0
-export JDK_HOME=/etc/alternatives/java_sdk_1.7.0
-export JRE_HOME=/etc/alternatives/java_sdk_1.7.0/jre
-export PATH=/etc/alternatives/java_sdk_1.7.0/bin:$PATH
-export SDK_HOME=/etc/alternatives/java_sdk_1.7.0
-export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
-export CLASSPATH="$CLASSPATH:/afs/.ist.utl.pt/users/3/4/ist177034/Documents/CNV/BIT:/afs/.ist.utl.pt/users/3/4/ist177034/Documents/CNV/BIT/samples:/afs/.ist.utl.pt/users/3/4/ist177034/Documents/CNV/test/src:./"
-
